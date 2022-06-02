71da3e0dd215e527cdcee3daa94ffb1667acfb8e
Jun Kwon <junhuk1113@naver.com>
2022-06-02 15:51:38 +0900

    Modify commentary

--- domitA.py
+++ domitA.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 import os
 path = os.path.join(os.path.dirname(__file__), 'chromedriver.exe')
-
 # selenium으로 무엇인가 입력하기 위한 import
 from selenium.webdriver.common.keys import Keys
-# 페이지 로딩을 기다리는데에 사용할 time 모듈 import
 import time
-# 크롬드라이버 실행  (경로 예: '/Users/Roy/Downloads/chromedriver')
 
 class autoSurvey():
     def __init__(self):
@@ -25,15 +22,11 @@ class autoSurvey():
                     self.user_data[tlist[0]] = tlist[1:]
                 break
                 
-        
-        #크롬 드라이버에 url 주소 넣고 실행
-
-    # 페이지가 완전히 로딩되도록 3초동안 기다림
-    #time.sleep(1)
     def runAuto(self,name, address, temperature):
-        driver = webdriver.Chrome(path) 
+        driver = webdriver.Chrome(path) # 크롬드라이버 실행
+        #크롬 드라이버에 url 주소 넣고 실행
         driver.get('https://form.office.naver.com/form/responseView.cmd?formkey=YzVhMWI5OTYtMmNhMC00YmI3LWFmMmYtNzQ3MjJlMzcyNjc2&sourceId=urlshare')
-        time.sleep(1)
+        time.sleep(1) #웹페이지 로딩 대기
         search_box = driver.find_element_by_xpath('//*[@id="answer"]')
         search_box.send_keys(address)
         search_box = driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/form/div/div[2]/div/div[3]/div/input')
