6c7edb7d9df285996fad5d518ff4169522f97bb2
Jun Kwon <junhuk1113@naver.com>
2022-06-02 15:14:08 +0900

    fix runtime error at selenium, fileIO

--- domitA.py
+++ domitA.py
@@ -10,22 +10,22 @@ import time
 
 class autoSurvey():
     def __init__(self):
-        self.driver = webdriver.Chrome(path) 
-        self.driver.get('https://form.office.naver.com/form/responseView.cmd?formkey=YzVhMWI5OTYtMmNhMC00YmI3LWFmMmYtNzQ3MjJlMzcyNjc2&sourceId=urlshare')
-        time.sleep(1)
         self.user_data = {}
         while True:
+            print("while..")
             try:
                 file = open('userData.dat','r',encoding='UTF-8')
             except FileNotFoundError:#userData.dat 파일이 없으면 새로 생성
+                print("except")
                 file = open('userData.dat','w',encoding='UTF-8')
                 file.close()
             else:
                 flist=file.readlines()
                 for i in flist:
+                    print("for loop")
                     tlist = i.split()
                     self.user_data[tlist[0]] = tlist[1:]
-                    break
+                break
                 
         
         #크롬 드라이버에 url 주소 넣고 실행
@@ -33,22 +33,27 @@ class autoSurvey():
     # 페이지가 완전히 로딩되도록 3초동안 기다림
     #time.sleep(1)
     def runAuto(self,name, address, temperature):
-        search_box = self.driver.find_element_by_xpath('//*[@id="answer"]')
+        print("runAuto method run")
+        driver = webdriver.Chrome(path) 
+        driver.get('https://form.office.naver.com/form/responseView.cmd?formkey=YzVhMWI5OTYtMmNhMC00YmI3LWFmMmYtNzQ3MjJlMzcyNjc2&sourceId=urlshare')
+        time.sleep(1)
+        search_box = driver.find_element_by_xpath('//*[@id="answer"]')
         search_box.send_keys(address)
-        search_box = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/form/div/div[2]/div/div[3]/div/input')
+        search_box = driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/form/div/div[2]/div/div[3]/div/input')
         search_box.send_keys(name) 
-        search_box = self.driver.find_element_by_xpath("/html/body/div[5]/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/form/div/div[3]/div/div[3]/div/input")
+        search_box = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/form/div/div[3]/div/div[3]/div/input")
         search_box.send_keys(temperature) 
         search_box.send_keys(Keys.PAGE_DOWN) 
-        search_box = self.driver.find_element_by_xpath('//*[@id="formItem_5"]/div/div[3]/div/div[2]/div')
+        search_box = driver.find_element_by_xpath('//*[@id="formItem_5"]/div/div[3]/div/div[2]/div')
         search_box.send_keys(Keys.ENTER) 
-        search_box = self.driver.find_element_by_xpath('//*[@id="pageNav"]/button[3]')
+        search_box = driver.find_element_by_xpath('//*[@id="pageNav"]/button[3]')
         search_box.send_keys(Keys.ENTER) 
         time.sleep(2)
-        self.driver.quit()
+        driver.quit()
     def createNewuser(self, name, address, temperature):
         self.user_data[name] = [address, temperature]
     def surveyAll(self):
+        print("surveyAll method run")
         for key in self.user_data.keys():
             self.runAuto(key,self.user_data[key][0],self.user_data[key][1])
 
