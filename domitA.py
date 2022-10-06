0a103b3808e72d93a55be3c349172e9e864ee6c8
junhuk1113 <junhuk1113@naver.com>
2022-10-07 02:02:53 +0900

    시간 변경

--- domitA.py
+++ domitA.py
@@ -56,7 +56,7 @@ class autoSurvey():
 
 Gugwon = autoSurvey()
 #Gugwon.surveyAll()
-schedule.every().day.at("01:49:00").do(Gugwon.surveyAll) 
+schedule.every().day.at("02:03:00").do(Gugwon.surveyAll) 
 while True:
     schedule.run_pending()
     time.sleep(1)
